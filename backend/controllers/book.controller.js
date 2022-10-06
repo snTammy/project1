114b17747a2b4191849a09c1d011ccb9c886cbd4
snTammy <94483189+snTammy@users.noreply.github.com>
2022-10-06 14:54:08 -0400

    GET functionality
    Get Book using Mongoose Id with ObjectId validation

--- backend/controllers/book.controller.js
+++ backend/controllers/book.controller.js
@@ -7,6 +7,17 @@ const findAllBooks = async () => {
     const books = await Books.find();
     return books;
 }
-
+//Find specific book
+const findBookById = async (id) => {
+    try{
+        const book = await Books.findById(id);
+        if(book == null){
+            throw {status:204, msg:`No book with the id ${id} was found.`}
+        }
+        return book; //Book is found
+    } catch (err) {
+        //Book not found
+    }
+};
 //export all our functions
-module.exports = {findAllBooks};
+module.exports = {findAllBooks, findBookById};
--- backend/routes/book.route.js
+++ backend/routes/book.route.js
@@ -1,18 +1,35 @@
 //handles our endpoint/CRUD functions
 const router = require('express').Router();
+const mongoose = require('mongoose');
 
 //import controller functions
-const { findAllBooks } = require('../controllers/book.controller.js');
+const { findAllBooks, findBookById } = require('../controllers/book.controller.js');
 
-
+// Validate ObjectId middleware
+const validateObjectId = (req, res, next) => {
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        res.status(204).send(); // Send back the response early
+    } else {
+        next(); // This calls the standard route for GET/POST/PUT/DELETE with (req, res)
+    }
+}
 //Get ALL Books
 //books
 router.get('/', async (req, res) =>{
     const books = await findAllBooks();
     res.json(books);
-})
+});
 //Get Book by Id
-
+router.get('/:id',validateObjectId, async (req, res) =>{
+    //req.params.id extracts the id number from the URL
+    try{
+        const book = await findBookById(req.params.id);
+        res.json(book);
+    } catch (err) {
+        //rejected promise = no book found
+        res.status(err?.status ?? 500).json(err);
+    }
+});
 //Create new Book
 
 //Update a Book
